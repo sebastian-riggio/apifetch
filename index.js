cf7f93fd4d06b32fa61c00ad777bcf6af466c5c1
sebastian-riggio <sebastian.riggio@gmail.com>
2023-08-05 16:41:42 +0200

    arreglo llamadaflags

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ function getPaises(done) {
     const results = fetch("https://restcountries.com/v3.1/all")
     results
      .then( response => response.json())
+    
      .then ( data => {
         done(data)
+        console.log(data)
      })
 
 }
@@ -13,7 +15,7 @@ getPaises (data => {
         const article = document.createRange().createContextualFragment(
             ` <article>
             <div class="image-container">
-              <img src="${pais.flag}" alt="bandera pais" />
+              <img src="${pais.flags.png}" alt="bandera pais" />
               <span> ${pais.name.common}</span>
             </div>
           </article>`);
